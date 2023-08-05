c8c53e50899bd3ae8388b96a0b2270870222821a
letsbuild-with-rajesh <86154498+letsbuild-with-rajesh@users.noreply.github.com>
2023-08-05 16:35:42 +0530

    Commit-6 Added search site feature

--- app/(home)/HomePage.tsx
+++ app/(home)/HomePage.tsx
@@ -1,22 +1,55 @@
 'use client'
 import Link from "next/link";
-import { useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { ISiteData } from "@/app/types";
 import Loader from "@/app/components/Loader";
 import SiteCard from "@/app/components/SiteCard";
 
-export default function HomePage({ mySpaceSites }: { mySpaceSites: Array<string> }) {
-  const [randomSiteDetails, setRandomSiteDetails] = useState<{ loading: boolean, data: ISiteData }>({
-    loading: false,
-    data: { API: '', Description: '', Link: '', Category: '' }
-  });
+const RANDOM_SITE_INIT_STATE = {
+  loading: false,
+  data: { API: '', Description: '', Link: '', Category: '' }
+};
 
+const SEARCH_RESULTS_INIT_STATE = {
+  loading: false,
+  data: []
+};
+
+
+export default function HomePage({ mySpaceSites }: { mySpaceSites: Array<string> }) {
+  const [randomSiteDetails, setRandomSiteDetails] = useState<{ loading: boolean, data: ISiteData }>(RANDOM_SITE_INIT_STATE);
+  const [searchInput, setSearchInput] = useState('');
+  const [searchResultsDetails, setSearchResultsDetails] = useState<{ loading: boolean, data: Array<ISiteData> }>(SEARCH_RESULTS_INIT_STATE);
 
   const getRandomSite = async () => {
+    setSearchResultsDetails(SEARCH_RESULTS_INIT_STATE);
     setRandomSiteDetails({ ...randomSiteDetails, loading: true });
     const res = await fetch('/api/randomsite', { next: { revalidate: 0 } });
     const { data } = await res.json();
     setRandomSiteDetails({ loading: false, data });
+  }
+
+  const getSearchResults = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setRandomSiteDetails(RANDOM_SITE_INIT_STATE);
+    setSearchResultsDetails({ ...searchResultsDetails, loading: true });
+    const titleRes = await fetch(`https://api.publicapis.org/entries?title=${searchInput}`);
+    const { entries: titleEntries } = await titleRes.json();
+
+    const descriptionRes = await fetch(`https://api.publicapis.org/entries?description=${searchInput}`);
+    const { entries: descriptionEntries } = await descriptionRes.json();
+
+    const entries: Array<ISiteData> = [];
+    const existingEntries: { [key: string]: boolean } = {};
+
+    [...titleEntries, ...descriptionEntries].map((site: ISiteData) => {
+      if (!existingEntries[site.Link]) {
+        existingEntries[site.Link] = true;
+        entries.push(site);
+      }
+    })
+
+    setSearchResultsDetails({ loading: false, data: entries });
   }
 
   return (
@@ -33,16 +66,29 @@ export default function HomePage({ mySpaceSites }: { mySpaceSites: Array<string>
           <Link className="nav-link active" href="/categories">Categories</Link>
         </li>
       </ul>
-      <div>
-        {randomSiteDetails.loading
-          ? <div className="m-4"><Loader containerHeightStyle="h-100" /></div>
-          : randomSiteDetails.data.API && (
-            <div className="d-flex justify-content-center m-4">
-              <SiteCard data={randomSiteDetails.data} isMySpaceSite={mySpaceSites.includes(randomSiteDetails.data.Link)} />
-            </div>
-          )
-        }
-      </div>
+      <form className="form-inline d-flex gap-3 m-3" onSubmit={getSearchResults}>
+        <input
+          className="form-control mr-sm-2"
+          type="search"
+          placeholder="Search site"
+          aria-label="Search site"
+          onChange={e => setSearchInput(e.target.value)}
+        />
+        <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+      {(randomSiteDetails.loading || searchResultsDetails.loading) && <div className="m-4"><Loader containerHeightStyle="h-100" /></div>}
+      {randomSiteDetails.data.API && (
+        <div className="d-flex justify-content-center m-4">
+          <SiteCard data={randomSiteDetails.data} isMySpaceSite={mySpaceSites.includes(randomSiteDetails.data.Link)} />
+        </div>
+      )}
+      {!!searchResultsDetails.data.length && (
+        <div className="d-flex flex-wrap justify-content-center gap-3 m-4">
+          {searchResultsDetails.data.map((site: ISiteData) => {
+            return <SiteCard data={site} isMySpaceSite={mySpaceSites.includes(site.Link)} />
+          })}
+        </div>
+      )}
     </div>
   )
 }
